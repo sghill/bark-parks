da6e55315c79a02299d2d833ff380688decb12cf
sghill <sghill.dev@gmail.com>
2013-03-30 18:00:41 -0400

    adding total number of locations to json response

--- toronto.rb
+++ toronto.rb
@@ -10,10 +10,11 @@ class Toronto < Sinatra::Base
 
   get "/parks" do
     content_type :json
-    response = {:locations => []}
     doc = Nokogiri::XML(File.read(File.join("resources", "locations-20110725.xml")))
     doc.remove_namespaces!
-    doc.xpath("//FacilityType[.='Leash-Free Area']").each do |facility_type|
+    leash_free_areas = doc.xpath("//FacilityType[.='Leash-Free Area']")
+    response = {:total => leash_free_areas.count, :locations => []}
+    leash_free_areas.each do |facility_type|
       location_xml = facility_type.parent.parent.parent
       response[:locations] << Location.new(location_xml.xpath(".//LocationID").text, location_xml.xpath(".//LocationName").text, location_xml.xpath(".//Address").text, location_xml.xpath(".//PostalCode").text)
     end
